0c7f65beeeb6f17b41978fd88b8be3b09e26db7c
Veggie Monster <vmonmoonshine@gmail.com>
2017-10-27 16:32:17 -0400

    fix to build

--- src/main/java/i2pouiconnection/Main.java
+++ src/main/java/i2pouiconnection/Main.java
@@ -20,7 +20,7 @@ import net.i2p.data.Destination;
 public class Main {
 
     // The name of the file to send.
-    String fileName = "hhgttg.txt";
+    static String fileName = "hhgttg.txt";
 
     public static void main(String[] args) {
         I2PSocketManager manager = I2PSocketManagerFactory.createManager();
@@ -57,22 +57,21 @@ public class Main {
             return;
         }
         try {
-	    String line = null;
-	    // FileReader reads text files in the default encoding.
-	    FileReader fileReader = new FileReader(fileName);
-		
-	    // Always wrap FileReader in BufferedReader.
-	    BufferedReader bufferedReader =
-		new BufferedReader(fileReader);
+            String line = null;
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = new FileReader(fileName);
+            
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            
+            //Write to server
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-	    //Write to server
-	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-
-	    while((line = bufferedReader.readLine()) != null) {
-		bw.write(line);
-		//Flush to make sure everything got sent
-		bw.flush();
-	    }
+            while((line = bufferedReader.readLine()) != null) {
+                bw.write(line);
+                //Flush to make sure everything got sent
+                bw.flush();
+            }
 	    
             //Read from server
             BufferedReader br2 = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -82,16 +81,15 @@ public class Main {
             }
             socket.close();
 
-	    // Always close files.
-	    bufferedReader.close();
+            // Always close files.
+            bufferedReader.close();
 
-        } catch (IOException ex) {
+        } catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + fileName + "'");
+        }
+        catch (IOException ex) {
             System.out.println("Error occurred while sending/receiving!");
-	}
-	catch(FileNotFoundException ex) {
-	    System.out.println(       "Unable to open file '" +
-				      fileName + "'");
-	}
+        }
 
     }
 
